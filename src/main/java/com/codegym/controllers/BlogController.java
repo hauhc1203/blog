b7a8e8272231ad6362e071ce538c137c830a0e83
hauhc1203 <hau.hc1203@gmail.com>
2022-07-25 14:42:07 +0700

    up

---
+++ src/main/java/com/codegym/controllers/BlogController.java
@@ -0,0 +1,105 @@
+package com.codegym.controllers;
+
+import com.codegym.models.Blogs;
+import com.codegym.models.Category;
+import com.codegym.models.Comment;
+import com.codegym.service.BlogService;
+import com.codegym.service.CategoryService;
+import com.codegym.service.CommentService;
+import com.codegym.validate.ValidateBlog;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+@Controller
+public class BlogController {
+    @Autowired
+    BlogService blogService;
+
+    @Autowired
+    CategoryService categoryService;
+
+    @Autowired
+    ValidateBlog validateBlog;
+
+    @Autowired
+    CommentService commentService;
+
+    @ModelAttribute(name = "blog")
+    public Blogs blogs(){
+        return new Blogs();
+    }
+
+    @ModelAttribute(name = "categories")
+    public List<Category> categories(){
+        return categoryService.getAll();
+    }
+
+    @GetMapping("/blogs")
+    public ModelAndView show(@RequestParam(defaultValue = "0") int page) {
+        ModelAndView modelAndView = new ModelAndView("home");
+        modelAndView.addObject("blogs", blogService.getAll(PageRequest.of(page, 2, Sort.by("date"))));
+        return modelAndView;
+    }
+
+    @PostMapping("/search")
+    public ModelAndView search(@RequestParam(defaultValue = "0") int page, @RequestParam String search) {
+        ModelAndView modelAndView = new ModelAndView("home");
+        modelAndView.addObject("blogs", blogService.getAllByTitle(PageRequest.of(page, 2, Sort.by("date")), search));
+        return modelAndView;
+    }
+
+    @GetMapping("/create")
+    public ModelAndView showCreate() {
+        ModelAndView modelAndView = new ModelAndView("create");
+        return modelAndView;
+    }
+    @GetMapping("/show")
+    public ModelAndView showBlog(@RequestParam long id) {
+        ModelAndView modelAndView = new ModelAndView("single-post");
+        modelAndView.addObject("comments",commentService.findAllByBlog(id));
+        modelAndView.addObject("blog_show", blogService.findById(id).get());
+        return modelAndView;
+    }
+    @PostMapping("/comment/{idBlog}")
+    public ModelAndView comment(Comment comment,@PathVariable("idBlog") long idBlog) {
+        Blogs blogs = blogService.findById(idBlog).get();
+        comment.setBlogs(blogs);
+        commentService.save(comment);
+        ModelAndView modelAndView = new ModelAndView("redirect:/show?id="+idBlog);
+        return modelAndView;
+    }
+
+    @PostMapping("/create")
+    public ModelAndView create(@Valid @ModelAttribute("blog") Blogs blogs,
+                               BindingResult bindingResult,
+                               @RequestParam MultipartFile upImg) {
+        validateBlog.validate(blogs,bindingResult);
+        if (bindingResult.hasFieldErrors()) {
+            ModelAndView modelAndView = new ModelAndView("create");
+            return modelAndView;
+        }
+        String nameImg = upImg.getOriginalFilename();
+        try {
+            FileCopyUtils.copy(upImg.getBytes(), new File("/Users/johntoan98gmail.com/Desktop/Blog/src/main/webapp/WEB-INF/img/" + nameImg));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        blogs.setImg("/img/" + nameImg);
+        blogService.save(blogs);
+        ModelAndView modelAndView = new ModelAndView("redirect:/blogs");
+        return modelAndView;
+    }
+
+}
---
+++ src/main/java/com/codegym/service/BlogService.java
@@ -0,0 +1,43 @@
+package com.codegym.service;
+
+import com.codegym.models.Blogs;
+import com.codegym.repository.IBlogRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class BlogService {
+    @Autowired
+    IBlogRepo iBlogRepo;
+
+    public Page<Blogs> getAll(Pageable pageable) {
+        return  iBlogRepo.findAll(pageable);
+    }
+    public Page<Blogs> getAllByTitle(Pageable pageable, String title) {
+        return  iBlogRepo.findByTitleContaining(pageable,title);
+    }
+
+    public void save(Blogs blogs) {
+        iBlogRepo.save(blogs);
+    }
+
+    public void delete(long id) {
+        iBlogRepo.deleteById(id);
+    }
+
+    public Optional<Blogs> findById(long id) {
+        return iBlogRepo.findById(id);
+    }
+
+    public Optional<Blogs> findByTitle(String title) {
+        return iBlogRepo.findByTitle(title);
+    }
+
+}
